fef918e23b7ca4a7051019387f9ffcc62a5a3f46
aero23h <huefner.kevin23@gmail.com>
2021-06-02 15:51:49 +0200

    Notenpunkte Umrechner

--- EPR_01_1.2.py
+++ EPR_01_1.2.py
@@ -1,12 +1,12 @@
 eingabe = input("Wie viel Punkte hast du? ")
 
-while not 0 <= int(eingabe) <= 15:
+while not 0 <= int(eingabe) <= 15:                             #Eingabe nicht 1-15
     print("Bitte nur Notenpunkte zwischen 1 und 15 eingeben")
     eingabe = input("Wie viel Punkte hast du nun? ")
 
-note = (17 - int(eingabe)) / 3
+note = (17 - int(eingabe)) / 3                                 #Umrechnung
 note = int(note.__round__(0))
-if 1 <= int(eingabe) <= 15:
+if 1 <= int(eingabe) <= 15:                                    #Note 1-5
     if int(eingabe) % 3 == 1:
         print("Deine Note ist:", str(note) + "-")
     elif int(eingabe) % 3 == 0:
@@ -14,5 +14,5 @@ if 1 <= int(eingabe) <= 15:
     else:
         print("Deine Note ist:", str(note))
 
-if int(eingabe) == 0:
+if int(eingabe) == 0:                                           #Note 6
     print("Deine Note ist:", str(note))
