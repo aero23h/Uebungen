ecb70fe8a74028cf1598f0b2967cc3361c2ee393
aero23h <huefner.kevin23@gmail.com>
2021-06-03 15:31:27 +0200

    Bool Formelrechner

---
+++ EPR_01_1.3.py
@@ -0,0 +1,36 @@
+eingabe = input("Gebe bitte eine  aussagenlogische Formel in Form ('Bool' 'Verknüpfung' 'Bool') ein: ")
+
+i = 0
+j = 0
+z = ""
+x = ""
+y = ""
+ergebnis = ""
+
+for i in range(0, len(eingabe)):
+    if eingabe[i] == chr(32):
+        x = eingabe[0:i]
+        break
+for j in range(i+1, len(eingabe)):
+    if eingabe[j] == chr(32):
+        y = eingabe[i+1:j]
+        z = eingabe[j+1:]
+        break
+
+if x == "True":
+    x = True
+elif x == "False":
+    x = False
+
+if z == "False":
+    z = False
+elif z == "True":
+    z = True
+
+if y == "and":
+    ergebnis = x and z
+if y == "or":
+    ergebnis = x or z
+
+print("Das Ergebnis deiner Formel ist:", ergebnis)
+
