59c8f9b503cbedcaae96be12e9aad1c1e5b362e9
aero23h <huefner.kevin23@gmail.com>
2021-06-12 15:03:08 +0200

    Arithmetischer Mittelwert -und Medianrechner

---
+++ EPR_02_2.2.2.py
@@ -0,0 +1,28 @@
+import math
+
+eingabe = input("Bitte gebe eine ganzzahlige Zahl ein: ")
+
+eingabe = int(eingabe)
+addiert = 0
+median = 0
+arit = 0
+
+if eingabe < 0:                                                         #wenn Eingabe negativ ist
+    for i in range(eingabe, 0 + 1):
+        addiert = addiert - i
+        arit = addiert / eingabe
+        median = arit
+
+if eingabe > 0:                                                         #wenn Eingabe posiiv ist
+    for i in range(1, eingabe + 1):
+        addiert = addiert + i
+        arit = addiert / eingabe
+        median = arit
+
+if median == int(median):                                               #kommas entfernen
+    median = int(median)
+
+if arit == int(arit):
+    arit = int(arit)
+
+print("Der Arithmetische Mittelwert ist:", arit, "und der Median:", median)
