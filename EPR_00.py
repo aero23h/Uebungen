f94745965278285494d19053fd4a70d693466f96
aero23h <huefner.kevin23@gmail.com>
2021-06-25 23:06:14 +0200

    rename

--- EPR_00.py
+++ EPR_00.py
@@ -1,4 +1,10 @@
-fahrenheit = input("welche Temperatur (in Fahrenheit) möchtest du umrechnen? ")
-fahrenheit = int(fahrenheit)
-celsius = (fahrenheit - 32) / 1.8
-print(fahrenheit, "°F",  "sind umgerechnet", celsius.__round__(2), "°C")
+#fahrenheit = input("welche Temperatur (in Fahrenheit) möchtest du umrechnen? ")
+#fahrenheit = int(fahrenheit)
+#celsius = (fahrenheit - 32) / 1.8
+#print(fahrenheit, "°F",  "sind umgerechnet", celsius.__round__(2), "°C")
+
+count = 0
+for i in range(10):
+    for j in range(i):
+        count = count + 1
+        print(count)
